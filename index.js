8fb45bc81767bf91eb174bd373f28919db2286de
Fr5quencya <mokhtararegood@gmail.com>
2026-06-09 23:27:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mineflayer = require('mineflayer')
 function createBot() {
   const bot = mineflayer.createBot({
     host: 'stormsmps4.play.hosting',
-    port: ,
+    port: 25575,
     username: 'stormycore',
     version: '1.20.1',
     auth: 'offline'
